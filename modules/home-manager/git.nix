1feeb06ca00fe429c6a1cd29ff8836f11e42358a
Guillaume-prog <76855158+Guillaume-prog@users.noreply.github.com>
2024-04-22 00:32:58 +0100

    improved git config

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -2,8 +2,14 @@
 
   programs.git = {
     enable = true;
+
     userName = "Guillaume-prog";
     userEmail = "76855158+Guillaume-prog@users.noreply.github.com";
+    
+    extraConfig = {
+      init.defaultBranch = "main";
+      push.autoSetupRemote = true;
+    };
   };
 
 }
