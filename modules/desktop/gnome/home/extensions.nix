993a27c5a9d705658ffc97b4482a682f8f7e1de0
Guillaume-prog <76855158+Guillaume-prog@users.noreply.github.com>
2024-07-31 20:49:02 +0100

    Modified top bar layout

--- modules/desktop/gnome/home/extensions.nix
+++ modules/desktop/gnome/home/extensions.nix
@@ -2,6 +2,7 @@
 
   home.packages = (with pkgs.gnomeExtensions; [
     appindicator
+    bluetooth-battery-meter
     blur-my-shell
     gsconnect
     user-themes
@@ -21,6 +22,14 @@
 
     "org/gnome/shell/extensions/vitals" = {
       hot-sensors = [ "_processor_usage_" "_memory_usage_" "__network-rx_max__" ];
+    };
+
+    "org/gnome/shell/extensions/appindicator" = {
+      tray-pos = "left";
+    };
+
+    "org/gnome/shell/extensions/Bluetooth-Battery-Meter" = {
+      enable-battery-level-text = true;
     };
 
   };
