bac6d7d9eb9864dccfa114069e07faa59cfc2ff2
Guillaume-prog <76855158+Guillaume-prog@users.noreply.github.com>
2025-02-26 08:06:19 +0100

    Modded ssh config

--- modules/home-manager/ssh.nix
+++ modules/home-manager/ssh.nix
@@ -1,15 +1,11 @@
 { ... }: {
   home.file.".ssh/config".text = ''
     Host hass
-      HostName 192.168.1.14
+      HostName 192.168.1.201
       User root
 
-    Host homelab
-      HostName 192.168.1.82
-      User lexi
-
     Host nas
-      HostName 192.168.1.48
+      HostName 192.168.1.200
       User yriel
       Port 9222
 
