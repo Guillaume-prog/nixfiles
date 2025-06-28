84d798473c34118ab14b6f2d3917b7e18d25131c
Guillaume-prog <76855158+Guillaume-prog@users.noreply.github.com>
2025-06-28 23:33:09 +0200

    Updated firefox config (buggy)

--- modules/home-manager/firefox/default.nix
+++ modules/home-manager/firefox/default.nix
@@ -27,18 +27,23 @@ in
       settings = {
         "browser.disableResetPrompt" = true;
         "browser.download.panel.shown" = true;
-        "browser.download.useDownloadDir" = false;
+        # "browser.download.useDownloadDir" = false;
 
         # bookmarks management
         "browser.bookmarks.file" = ./bookmarks.html;
         "browser.places.importBookmarksHTML" = true;
         "browser.bookmarks.addedImportButton" = false;
+        "browser.bookmarks.restore_default_bookmarks" = false;
+        "browser.places.importDefaults" = false;
 
         # homepage
         "browser.search.defaultenginename" = defaultSearchEngine;
         "browser.search.order.1" = defaultSearchEngine;
 
         "browser.startup.homepage" = "https://start.duckduckgo.com";
+        "browser.startup.page" = 3; # show previous tabs
+        "browser.sessionstore.resume_session_once" = false;
+        "browser.sessionstore.resume_from_crash" = true;
 
         # newtab config
         "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
@@ -48,17 +53,43 @@ in
         "browser.shell.checkDefaultBrowser" = false;
         "browser.shell.defaultBrowserCheckCount" = 1;
 
+        # topbar customisation
+        "browser.uiCustomization.state" = lib.importJSON ./firefox-ui.json;
+
+        # Auto allow extensions
+        "extensions.autoDisableScopes" = 0;
+        "extensions.enabledScopes" = 15; # Enable all install locations (0b1111)
+
+        # Force dark mode
+        "ui.systemUsesDarkTheme" = 1;
+        # "layout.css.prefers-color-scheme.content-override" = 2;
+
         # privacy & security settings
         "dom.security.https_only_mode" = true;
         "identity.fxaccounts.enabled" = false;
         "privacy.trackingprotection.enabled" = true;
         "signon.rememberSignons" = false;
 
-        # topbar customisation
-        "browser.uiCustomization.state" = lib.importJSON ./firefox-ui.json;
+        "datareporting.healthreport.uploadEnabled" = false;
+        "datareporting.policy.dataSubmissionEnabled" = false;
+        "toolkit.telemetry.enabled" = false;
+        "toolkit.telemetry.unified" = false;
+        "toolkit.telemetry.archive.enabled" = false;
+        "toolkit.telemetry.newProfilePing.enabled" = false;
+        "toolkit.telemetry.shutdownPingSender.enabled" = false;
+        "toolkit.telemetry.updatePing.enabled" = false;
+        "toolkit.telemetry.bhrPing.enabled" = false;
+        "toolkit.telemetry.firstShutdownPing.enabled" = false;
 
-        # Auto allow extensions i think ???
-        "extensions.InstallTriggerImpl.enabled" = true;
+        "browser.ping-centre.telemetry" = false;
+        "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+        "browser.newtabpage.activity-stream.telemetry" = false;
+
+        "experiments.enabled" = false;
+        "experiments.supported" = false;
+        "network.cookie.cookieBehavior" = 1; # block third-party (or use 5 for Total Cookie Protection)
+        "privacy.firstparty.isolate" = true;
+        "privacy.resistFingerprinting" = true;
       };
 
       search = {
