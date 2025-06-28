1fabc9979879b01cdebc75b93bd959db60c3720a
Guillaume-prog <76855158+Guillaume-prog@users.noreply.github.com>
2025-06-28 23:33:43 +0200

    Cleanup and move of state Version

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -17,6 +17,7 @@
     ./screen-share.nix
   ];
 
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nix.nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
@@ -72,13 +73,5 @@
   };
 
   screen-share.enable = true;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
 
 }
