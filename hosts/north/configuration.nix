72d28d55c1b8d93bf604178df3157fb66bd3ff3c
Guillaume-prog <76855158+Guillaume-prog@users.noreply.github.com>
2025-07-01 00:49:25 +0200

    feat(hw): Added gpu configuration

--- hosts/north/configuration.nix
+++ hosts/north/configuration.nix
@@ -21,6 +21,15 @@
   # Keyboard layout
   keyboard.layout = "fr";
 
+  # enable GPU
+  services.xserver.enable = true;
+  services.xserver.videoDrivers = [ "amdgpu" ];
+
+  hardware.graphics = {
+  enable = true;
+  enable32Bit = true;
+};
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
