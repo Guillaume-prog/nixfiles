07bff2146436438d88421b8f7b9cc4859a9b0555
Guillaume-prog <76855158+Guillaume-prog@users.noreply.github.com>
2025-11-24 14:26:30 +0100

    Changes from pavilion

--- modules/nixos/bootloader.nix
+++ modules/nixos/bootloader.nix
@@ -1,32 +1,48 @@
-{ inputs, lib, config, ... }: 
+{ inputs, lib, config, ... }:
 
 let 
   themePaths = {
     catpuccin-macchiato = "${inputs.catpuccin-grub}/src/catppuccin-macchiato-grub-theme";
   };
 
-  cfg = config.bootloader;
+  cfg = config.my.bootloader;
 in {
-  options.bootloader = with lib; {
+  options.my.bootloader = with lib; {
+    enable = mkEnableOption "Custom bootloader";
+
+    bootMode = mkOption {
+      type = types.enum [ "legacy" "uefi" ];
+      default = "uefi";
+      description = "Boot mode: legacy MBR or modern UEFI";
+    };
+
+    device = mkOption {
+      type = types.str;
+      default = "nodev";
+      description = "Device for legacy MBR installs. Ignored for UEFI.";
+    };
+
     theme = mkOption {
       type = types.str;
       default = "catpuccin-macchiato";
-      example = "catpuccin-macchiato";
-      description = "Which theme to use on startup. Current options are: catpuccin, sleek-dark"; 
     };
   };
 
-  config = {
-    # Bootloader.
-    boot.loader.efi.canTouchEfiVariables = true;
+  config = lib.mkIf cfg.enable {
     boot.loader.systemd-boot.enable = false;
-    
+
+    # Legacy BIOS mode
     boot.loader.grub = {
       enable = true;
       useOSProber = true;
-      device = "nodev";
-      efiSupport = true;
       theme = themePaths.${cfg.theme};
+
+      device = if cfg.bootMode == "legacy" then cfg.device else "nodev";
+      efiSupport = cfg.bootMode == "uefi";
+      efiInstallAsRemovable = cfg.bootMode == "uefi";
     };
+
+    # Only relevant for UEFI.
+    boot.loader.efi.canTouchEfiVariables = (cfg.bootMode == "uefi");
   };
 }
