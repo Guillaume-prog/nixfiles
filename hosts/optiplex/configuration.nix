c00215de72c5825bd933ddf67208089297610231
Guillaume-prog <76855158+Guillaume-prog@users.noreply.github.com>
2026-05-03 12:44:00 +0200

    🚀 New machine : optiplex

---
+++ hosts/optiplex/configuration.nix
@@ -0,0 +1,72 @@
+{ pkgs, config, ... }: {
+
+  imports = [
+    ./hardware-configuration.nix
+    ../../modules/nixos/bootloader.nix
+    ../../modules/nixos/nh.nix
+    ../../modules/nixos/sops.nix
+  ];
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  # Bootloader.
+  my.bootloader = {
+    enable = true;
+    bootMode = "legacy";
+    device = "/dev/sda";
+  };
+
+  # Enable networking
+  networking.hostName = "optiplex";
+  networking.networkmanager.enable = true;
+
+  # Headless mode
+  services.xserver.enable = false;
+  services.logind.settings.Login.HandleLidSwitch = "ignore";
+  console.keyMap = "fr";
+
+  # SSH
+  services.openssh.enable = true;
+  networking.firewall.allowedTCPPorts = [22];
+
+  users.users.lexi = {
+    isNormalUser = true;
+    description = "lexi";
+    extraGroups = ["networkmanager" "wheel" "docker"];
+  };
+
+  environment.systemPackages = with pkgs; [ 
+    cifs-utils
+    fastfetch 
+    vim 
+    tree 
+    lazydocker 
+    python312 
+  ];
+
+  virtualisation.docker.enable = true;
+
+  networking.extraHosts = ''
+    192.168.1.200 nas.lan
+    192.168.1.201 potato.lan # SELF
+    192.168.1.202 home-assistant.lan
+    192.168.1.203 3d-printer.lan
+  '';
+
+  # Mount the NAS share
+  environment.etc."smb-credentials".source = config.sops.secrets."smb-credentials".path;
+
+  fileSystems."/mnt/nas" = {
+    device = "//nas.lan/media";
+    fsType = "cifs";
+
+    # Credentials
+    options = let
+      # this line prevents hanging on network split
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+    in ["${automount_opts},credentials=/etc/smb-credentials"];
+  };
+
+  system.stateVersion = "24.05";
+
+}
