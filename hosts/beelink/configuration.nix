e00726093a98a82a7d9c89b74fba20fa9bac5c7c
Guillaume-prog <76855158+Guillaume-prog@users.noreply.github.com>
2026-06-11 16:32:06 +0200

    🐛 Fixed bootloader issues on beelink

--- hosts/beelink/configuration.nix
+++ hosts/beelink/configuration.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
 imports = [
@@ -8,11 +8,23 @@ imports = [
 
   networking.hostName = "beelink";
 
+  # bootloader stuff
+  boot.loader.grub.efiInstallAsRemovable = lib.mkForce false;
+  boot.loader.efi.canTouchEfiVariables = true;
+
   environment.systemPackages = with pkgs; [ nicotine-plus  beets ];
   
   user.guillaume = {
     enable = true;
     home-config = ../../users/guillaume/home.nix;
+  };
+
+  services.displayManager = {
+    gdm.enable = true;
+    autoLogin =  {
+      enable = true;
+      user = "guillaume";
+    };
   };
 
   # Keyboard layout
