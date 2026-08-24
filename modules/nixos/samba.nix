7eb3f0623f2a893e2a8ca749b7634d2a9196cadc
Guillaume-prog <76855158+Guillaume-prog@users.noreply.github.com>
2026-08-24 18:12:18 +0200

    ✨ Patched up samba config

--- modules/nixos/samba.nix
+++ modules/nixos/samba.nix
@@ -1,29 +1,34 @@
 { pkgs, lib, config, ... }: let
+  cfg = config.my.samba;
   samba-host="192.168.1.200";
+
+  samba-fs = enable: path: lib.mkIf enable {
+    device = "//${samba-host}/${path}";
+    fsType = "cifs";
+
+    # Credentials
+    options = let
+      # this line prevents hanging on network split
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+      permissions_opts = "uid=1000,gid=100";
+    in ["${automount_opts},${permissions_opts},credentials=/etc/smb-credentials"];
+  };
 in {
 
   options.my.samba = {
-    enable = lib.mkEnableOption "Mount the NAS share";
+    nas.enable = lib.mkEnableOption "Mount the NAS share";
+    pictures.enable = lib.mkEnableOption "Mount the pictures share";
   };
 
-  config = lib.mkIf config.my.samba.enable {
+  config = lib.mkIf (cfg.nas.enable || cfg.pictures.enable) {
     
     environment.systemPackages = [ pkgs.cifs-utils ];
 
     # Mount the NAS share
     environment.etc."smb-credentials".source = config.sops.secrets."smb-credentials".path;
 
-    fileSystems."/mnt/nas" = {
-      device = "//${samba-host}/media";
-      fsType = "cifs";
-
-      # Credentials
-      options = let
-        # this line prevents hanging on network split
-        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-        permissions_opts = "uid=1000,gid=100";
-      in ["${automount_opts},${permissions_opts},credentials=/etc/smb-credentials"];
-    };
+    fileSystems."/mnt/nas" = samba-fs cfg.nas.enable "media";
+    fileSystems."/mnt/pictures" = samba-fs cfg.pictures.enable "pictures";
 
   };
 
