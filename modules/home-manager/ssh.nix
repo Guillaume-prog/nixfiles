93ae8766dc0b3b89d3c8527fc16473b0552b7b36
Guillaume-prog <76855158+Guillaume-prog@users.noreply.github.com>
2026-08-24 23:35:13 +0200

    Modified ssh shortcuts

--- modules/home-manager/ssh.nix
+++ modules/home-manager/ssh.nix
@@ -1,17 +1,17 @@
 {...}: {
   home.file.".ssh/config".text = ''
-    Host hass
-      HostName 192.168.1.201
-      User root
-
     Host nas
       HostName 192.168.1.200
       User truenas_admin
       Port 9222
 
-    Host potato
+    Host optiplex
       HostName 192.168.1.201
       User lexi
+
+    Host hass
+      HostName 192.168.1.202
+      User root
 
     Host vps
       HostName 82.165.119.207
